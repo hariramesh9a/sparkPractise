2b9082fb25bd4bb8ce655f5a3460357447c22748
hariramesh9a <akbank05>
2020-03-07 15:15:00 -0600

    added more code

--- src/main/scala/DBExcerciseUDF.scala
+++ src/main/scala/DBExcerciseUDF.scala
@@ -1,6 +1,9 @@
+import java.sql.Timestamp
+import java.time.{LocalDateTime, Period}
+
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.functions.{count, explode, substring, row_number}
+import org.apache.spark.sql.functions.{count, datediff, explode, row_number, split, substring, udf}
 
 object DBExcerciseUDF {
 
@@ -10,19 +13,36 @@ object DBExcerciseUDF {
     val sc = new SparkContext(conf)
     val dataDir = "/Users/harumughan/IdeaProjects/spark_examples/data/"
     val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
 
     val df = sqlContext.read.option("inferSchema", "true").option("header", "true").option("delimiter", "|").csv(dataDir + "test.dat")
     df.show()
 
-//    Below we've created a small DataFrame. You should use DataFrame API functions and UDFs to accomplish two tasks.
-//
-//    You need to parse the State and city into two different columns.
-//    You need to get the number of days in between the start and end dates. You need to do this two ways.
-//      Firstly, you should use SparkSQL functions to get this date difference.
-//      Secondly, you should write a udf that gets the number of days between the end date and the start date.
+    df.printSchema()
 
-    df.withColumn("city")
+    //    Below we've created a small DataFrame. You should use DataFrame API functions and UDFs to accomplish two tasks.
+    //
+    //    You need to parse the State and city into two different columns.
+    //    You need to get the number of days in between the start and end dates. You need to do this two ways.
+    //      Firstly, you should use SparkSQL functions to get this date difference.
+    //      Secondly, you should write a udf that gets the number of days between the end date and the start date.
 
+    df.withColumn("state", split($"location", "-")(0)).
+      withColumn("city", split($"location", "-")(1)).show()
+
+
+    //    No of days
+
+    df.withColumn("datediff", datediff($"end_date", 'start_date)).show()
+
+
+    //    UDF
+
+    val difference = udf((end: Timestamp, start: Timestamp) => {
+      (end.getTime - start.getTime) / (24 * 60 * 60 * 1000)
+    })
+
+    df.withColumn("time_diff", difference('end_date, 'start_date)).show()
 
   }
 
